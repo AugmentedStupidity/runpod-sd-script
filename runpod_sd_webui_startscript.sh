74de507417ce89162e18402bfa454263a217343d
AugmentedStupidity <134770379+AugmentedStupidity@users.noreply.github.com>
2023-06-06 10:37:40 -0400

    Update runpod_sd_webui_startscript.sh

--- runpod_sd_webui_startscript.sh
+++ runpod_sd_webui_startscript.sh
@@ -49,7 +49,7 @@ rm Realistic_Vision_V1.4-*
 #bash webui.sh --xformers --api --share
 
 wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --nightly-channel --claim-token KvsmT6WV9cuYelOVZ-CEJtIkoUkleSlI2qtSzwBOB0f7InqJFOv_ZSholDuzxk-3dtoeOsJM8Ky3RJ9j7ubgKgK1MrwbNkmEDNaTPEsUKcoA1XuMTBgafROPKf5lS-KubYeLDp8 --claim-rooms 08add65d-2e24-4137-b1cc-ad05f57e9dcd --claim-url https://app.netdata.cloud
-&&
+
 systemctl restart netdata
 
 cd ~/text-generation-webui/
