cdece393692c4ac2344c8a185f696243122e94b5
AugmentedStupidity <134770379+AugmentedStupidity@users.noreply.github.com>
2023-06-06 16:52:39 -0400

    Create start-sd.sh

---
+++ start-sd.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# 
+if dpkg-query -W libgl1 2>/dev/null | grep -q "libgl1"; then
+  echo -n ""
+else
+  echo "libgl1 is not installed. Installing..."
+  apt update
+  apt install -yq libgl1
+fi
+
+# 
+#if dpkg-query -W ngrok 2>/dev/null | grep -q "ngrok"; then
+#  echo -n ""
+#else
+#  echo "ngrok is not installed. Installing..."
+#  curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
+#  cp /workspace/ngrok.yml /root/.config/ngrok/ngrok.yml
+#fi
+
+if [[ -z "${VIRTUAL_ENV}" ]]; then
+  echo "python venv."
+  source /workspace/stable-diffusion-webui/venv/bin/activate
+else
+  echo "venv . ."
+fi
+
+nohup ngrok http 7861 --log=stdout &> /dev/null &
+
+NGROK_PUBLIC_URL=""
+while [ -z "$NGROK_PUBLIC_URL" ]; do
+  # Run 'curl' against ngrok API and extract public (using 'sed' command)
+  export NGROK_PUBLIC_URL=$(curl --silent --max-time 10 --connect-timeout 5 \
+                            --show-error http://127.0.0.1:4040/api/tunnels | \
+                            sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p')
+  sleep 1
+  echo -n "."
+done
+
+function start_webui {
+  local message=${1}
+  local duration=${2}
+  local columns=$(tput cols)
+  local spacing=$(( $columns / 3 ))
+  local progress=$(echo "scale=2; $spacing/$duration" | bc)
+  local i=0
+  while true
+  do
+    echo -ne "\e[42;1;31mNgrok 터널이 열렸습니다: https://${message}/\e[0m"
+    local count=0
+    while [ $count -lt $spacing ]
+    do
+      #echo -n ""
+      (( count ++ ))
+    done
+    #echo -n ""
+    local j=0
+    while [ $j -lt $i ]
+    do
+      #echo -n ""
+      (( j ++ ))
+    done
+    echo -ne "\r" >&2 # 표준 에러 출력으로 변경
+    sleep 1
+    (( i ++ ))
+    if [ $i -eq $duration ]
+    then
+      i=0
+    fi
+  done
+}
+
+#
+start_webui "${NGROK_PUBLIC_URL}" 1 &
+python /workspace/stable-diffusion-webui/launch.py --listen --xformers
+
+# 
+kill $!
+kill -9 $(ps -ef | grep 'ngrok' | grep -v 'grep' | awk '{print $2}')
+deactivate
+echo "ngrok stopped"
+echo "."
