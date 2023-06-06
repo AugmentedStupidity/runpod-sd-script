3c9f3efbd7b92190fdbe6c66180944c003c7e709
AugmentedStupidity <134770379+AugmentedStupidity@users.noreply.github.com>
2023-06-06 14:46:57 -0400

    Update runpod_sd_webui_startscript.sh

--- runpod_sd_webui_startscript.sh
+++ runpod_sd_webui_startscript.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 apt update
-# apt install libgl1 -yq
+apt install libgl1 -yq
 apt update && apt install -y nano ffmpeg neofetch glances htop wget git python3 python3-venv pip ocl-icd-libopencl1 nvidia-cuda-toolkit hashcat hcxdumptool && ffmpeg -version
 # Ngrok
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
+#curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
+#https://ngrok.com/download
+curl --header 'Host: bin.equinox.io' --user-agent 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --referer 'https://dashboard.ngrok.com/' --header 'Upgrade-Insecure-Requests: 1' --header 'Sec-Fetch-Dest: document' --header 'Sec-Fetch-Mode: navigate' --header 'Sec-Fetch-Site: cross-site' --header 'Sec-Fetch-User: ?1' 'https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz' --output 'ngrok-v3-stable-linux-amd64.tgz'
+
+tar -xvzf ngrok-v3-stable-linux-amd64.tgz
+
+sudo mv ./ngrok /usr/bin/ngrok
 # Ngrok 
 ngrok config add-authtoken NGROK_TOKEN_HERE
 echo "authtoken: NGROK_TOKEN_HERE" > /workspace/ngrok.yml
@@ -19,17 +25,17 @@ wget https://huggingface.co/datasets/gsdf/EasyNegative/resolve/main/EasyNegative
 # 
 git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git /workspace/stable-diffusion-webui/extensions/a1111-sd-webui-tagcomplete/
 # venv
-python -m venv /workspace/stable-diffusion-webui/venv --without-pip
+#python -m venv /workspace/stable-diffusion-webui/venv --without-pip
 # venv activate
-source /workspace/stable-diffusion-webui/venv/bin/activate
-curl https://bootstrap.pypa.io/get-pip.py | python
+#source /workspace/stable-diffusion-webui/venv/bin/activate
+#curl https://bootstrap.pypa.io/get-pip.py | python
 # 
-python -m pip install -r /workspace/stable-diffusion-webui/requirements_versions.txt
+#python -m pip install -r /workspace/stable-diffusion-webui/requirements_versions.txt
 # start.sh 
-wget https://gist.github.com/panta5/b0cbddee37d6839561ad1b6a2d353a5b/raw/start.sh -O /workspace/stable-diffusion-webui/start.sh
-wget https://gist.githubusercontent.com/panta5/6239382f45a773d75a9788efd1c940a9/raw/continue.sh -O /workspace/stable-diffusion-webui/continue.sh
-chmod +x /workspace/stable-diffusion-webui/start.sh
-chmod +x /workspace/stable-diffusion-webui/continue.sh
+#wget https://gist.github.com/panta5/b0cbddee37d6839561ad1b6a2d353a5b/raw/start.sh -O /workspace/stable-diffusion-webui/start.sh
+#wget https://gist.githubusercontent.com/panta5/6239382f45a773d75a9788efd1c940a9/raw/continue.sh -O /workspace/stable-diffusion-webui/continue.sh
+#chmod +x /workspace/stable-diffusion-webui/start.sh
+#chmod +x /workspace/stable-diffusion-webui/continue.sh
 # 
 ###############################################################################################
 
@@ -82,26 +88,26 @@ ls -la
 
 
 #=================================
-sed -i "/#fi/i \
-alias restartob='bash /restart-text-generation-webui.sh' \
-alias editmodel='nano /workspace/run-text-generation-webui.sh' \
-alias restartsd='bash /workspace/stable-diffusion-webui/webui.sh --xformers --api --share' \
-alias aliasedit='nano ~/.bashrc' \
-alias aliasreload='source ~/.bashrc' \
-alias pkill='kill -9' \
-alias netdatainstall='wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --nightly-channel --claim-token KvsmT6WV9cuYelOVZ-CEJtIkoUkleSlI2qtSzwBOB0f7InqJFOv_ZSholDuzxk-3dtoeOsJM8Ky3RJ9j7ubgKgK1MrwbNkmEDNaTPEsUKcoA1XuMTBgafROPKf5lS-KubYeLDp8 --claim-rooms 08add65d-2e24-4137-b1cc-ad05f57e9dcd --claim-url https://app.netdata.cloud' \
-alias netdatarestart='service netdata restart' \
-/g" ~/.bashrc
+sed -i "/#fi/i \n
+alias restartob='bash /restart-text-generation-webui.sh' \n
+alias editmodel='nano /workspace/run-text-generation-webui.sh' \n
+alias restartsd='bash /workspace/stable-diffusion-webui/webui.sh --xformers --api --share' \n
+alias aliasedit='nano ~/.bashrc' \n
+alias aliasreload='source ~/.bashrc' \n
+alias pkill='kill -9' \n
+alias netdatainstall='wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --nightly-channel --claim-token KvsmT6WV9cuYelOVZ-CEJtIkoUkleSlI2qtSzwBOB0f7InqJFOv_ZSholDuzxk-3dtoeOsJM8Ky3RJ9j7ubgKgK1MrwbNkmEDNaTPEsUKcoA1XuMTBgafROPKf5lS-KubYeLDp8 --claim-rooms 08add65d-2e24-4137-b1cc-ad05f57e9dcd --claim-url https://app.netdata.cloud' \n
+alias netdatarestart='service netdata restart'/g" ~/.bashrc
+
 source ~/.bashrc
 
-alias restart='bash /restart-text-generation-webui.sh'
-alias editmodel='nano /workspace/run-text-generation-webui.sh'
-alias restartsd='bash /workspace/stable-diffusion-webui/webui.sh --xformers --api --share'
-alias aliasedit='nano ~/.bashrc'
-alias aliasreload='source ~/.bashrc'
-alias pkill='kill -9'
-alias netdatainstall='wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --nightly-channel --claim-token KvsmT6WV9cuYelOVZ-CEJtIkoUkleSlI2qtSzwBOB0f7InqJFOv_ZSholDuzxk-3dtoeOsJM8Ky3RJ9j7ubgKgK1MrwbNkmEDNaTPEsUKcoA1XuMTBgafROPKf5lS-KubYeLDp8 --claim-rooms 08add65d-2e24-4137-b1cc-ad05f57e9dcd --claim-url https://app.netdata.cloud'
-alias netdatarestart='service netdata restart'
+#alias restart='bash /restart-text-generation-webui.sh'
+#alias editmodel='nano /workspace/run-text-generation-webui.sh'
+#alias restartsd='bash /workspace/stable-diffusion-webui/webui.sh --xformers --api --share'
+#alias aliasedit='nano ~/.bashrc'
+#alias aliasreload='source ~/.bashrc'
+#alias pkill='kill -9'
+#alias netdatainstall='wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --nightly-channel --claim-token KvsmT6WV9cuYelOVZ-CEJtIkoUkleSlI2qtSzwBOB0f7InqJFOv_ZSholDuzxk-3dtoeOsJM8Ky3RJ9j7ubgKgK1MrwbNkmEDNaTPEsUKcoA1XuMTBgafROPKf5lS-KubYeLDp8 --claim-rooms 08add65d-2e24-4137-b1cc-ad05f57e9dcd --claim-url https://app.netdata.cloud'
+#alias netdatarestart='service netdata restart'
 source ~/.bashrc
 
 #=================================
@@ -109,8 +115,8 @@ source ~/.bashrc
 ###############################################################################################
 apt-get clean
 sed -i 's/can_run_as_root=0/can_run_as_root=1/g' /workspace/stable-diffusion-webui/webui.sh
-echo -e "/workspace/stable-diffusion-webui/start.sh"
-echo -e "\n\e[42;1;31mbash -c \"cd /workspace/stable-diffusion-webui \&\& ./continue.sh\"\e[0m "
+echo "bash -c /workspace/stable-diffusion-webui/start.sh"
+echo "bash -c /workspace/stable-diffusion-webui/continue.sh   \"\e[0m "
 
 cd /workspace/stable-diffusion-webui/
 bash webui.sh --xformers --api --share
