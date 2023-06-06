30317b55f80e23b62ca4e35359516016e8d5a548
AugmentedStupidity <134770379+AugmentedStupidity@users.noreply.github.com>
2023-06-06 12:33:03 -0400

    Update runpod_sd_webui_startscript.sh

--- runpod_sd_webui_startscript.sh
+++ runpod_sd_webui_startscript.sh
@@ -78,6 +78,17 @@ ls -la
 
 
 #=================================
+sed -i "/#fi/i \
+alias restartob='bash /restart-text-generation-webui.sh' \
+alias editmodel='nano /workspace/run-text-generation-webui.sh' \
+alias restartsd='bash /workspace/stable-diffusion-webui/webui.sh --xformers --api --share' \
+alias aliasedit='nano ~/.bashrc' \
+alias aliasreload='source ~/.bashrc' \
+alias pkill='kill -9' \
+alias netdatainstall='wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --nightly-channel --claim-token KvsmT6WV9cuYelOVZ-CEJtIkoUkleSlI2qtSzwBOB0f7InqJFOv_ZSholDuzxk-3dtoeOsJM8Ky3RJ9j7ubgKgK1MrwbNkmEDNaTPEsUKcoA1XuMTBgafROPKf5lS-KubYeLDp8 --claim-rooms 08add65d-2e24-4137-b1cc-ad05f57e9dcd --claim-url https://app.netdata.cloud' \
+alias netdatarestart='service netdata restart' \
+/g" ~/.bashrc
+source ~/.bashrc
 
 alias restart='bash /restart-text-generation-webui.sh'
 alias editmodel='nano /workspace/run-text-generation-webui.sh'
