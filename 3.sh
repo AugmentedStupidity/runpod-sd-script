d22f2c2b6e08ecf30ee59fc7596aac50d1e2c102
AugmentedStupidity <134770379+AugmentedStupidity@users.noreply.github.com>
2023-06-06 19:40:20 -0400

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -24,11 +24,15 @@ wget https://huggingface.co/datasets/gsdf/EasyNegative/resolve/main/EasyNegative
 git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git /workspace/stable-diffusion-webui/extensions/a1111-sd-webui-tagcomplete/
 # venv
 #python -m venv /workspace/stable-diffusion-webui/venv --without-pip
+python -m venv /workspace/stable-diffusion-webui/venv
 # venv activate
 #source /workspace/stable-diffusion-webui/venv/bin/activate
+source /workspace/stable-diffusion-webui/venv/bin/activate
 #curl https://bootstrap.pypa.io/get-pip.py | python
 # 
 #python -m pip install -r /workspace/stable-diffusion-webui/requirements_versions.txt
+python -m pip install -r /workspace/stable-diffusion-webui/requirements.txt
+
 # start.sh 
 wget https://raw.githubusercontent.com/AugmentedStupidity/runpod-sd-script/main/start-sd.sh -O /workspace/stable-diffusion-webui/start-sd.sh
 wget https://raw.githubusercontent.com/AugmentedStupidity/runpod-sd-script/main/continue.sh -O /workspace/stable-diffusion-webui/continue.sh
